fad1242a3af1ac8a1e721203f057f7f6132f2b05
utsavbalar1231 <utsavbalar1231@gmail.com>
2025-12-12 23:43:26 +0530

    chore(stage-distiller): Move to stable distribution

--- stage-distiller/00-install-package-dependencies/00-run-chroot.sh
+++ stage-distiller/00-install-package-dependencies/00-run-chroot.sh
@@ -7,7 +7,7 @@ echo "Configuring APT repositories..."
 curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg
 echo "deb https://debian.griffo.io/apt ${RELEASE} main" | tee /etc/apt/sources.list.d/debian.griffo.io.list
 curl -sS https://apt.pamir.ai/pamir-ai.gpg | gpg --dearmor --yes -o /usr/share/keyrings/pamir-ai-archive-keyring.gpg
-echo "deb [arch=arm64 signed-by=/usr/share/keyrings/pamir-ai-archive-keyring.gpg] https://apt.pamir.ai/ testing main" | tee /etc/apt/sources.list.d/pamir-ai.list
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/pamir-ai-archive-keyring.gpg] https://apt.pamir.ai/ stable main" | tee /etc/apt/sources.list.d/pamir-ai.list
 apt-get update
 apt-get install uv -y
 
